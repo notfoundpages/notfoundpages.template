cfec386479f35e1081764fde5ca1b142425565c5
Virati Akiranandhan Reddy <viratiaki53@gmail.com>
2026-07-21 16:31:36 +0530

    style.css

--- style.css
+++ style.css
@@ -1,9 +1,9 @@
 /* 
-© YYYY Not Found Pages · 404-*
+© YYYY Not Found Pages · 404-
 Released under the MIT License
 
-Repository → https://github.com/notfoundpages/404-*
-Live Preview → https://notfoundpages.github.io/404-*
+Repository → https://github.com/notfoundpages/404-
+Live Preview → https://notfoundpages.github.io/404-
 
 Discover the full collection → https://notfoundpages.github.io
 */
